7b579f239704864ae29d3c8329da1e1858584742
ben zina mohamed elyes <benzina.mohamedelyes@gmail.com>
2021-01-20 20:29:30 +0100

    Giving an initialized grid (5X5) when i set multiple living cell with the coordinates (1,1) and (2,2) then we should have 1 adjacent living cell for the coordinates (0,1) and 2 living cells for the coordinates (2,1) on the grid: Test OK

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -67,7 +67,7 @@ namespace GameOfLife
                     (x+1, y-1),
                     (x+1, y),
                     (x+1, y+1),
-                    (x+1, y+1)
+                    (x, y+1)
                 };
                 foreach(var coordinate in listOfCoordinatesToCheck)
                 {
--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -58,5 +58,20 @@ namespace Tests
             Program.CountAdjacentLivingCells(2, 1).Should().Be(1);
 
         }
+        [Fact]
+        public void ShouldFindMultipleLivingAdjacentCell()
+        {
+            string testString = "5 5";
+            Program.VerifyGridSize(testString).Should().BeTrue();
+
+            Program.InitializeGridWithDeadCells();
+
+            Program.SetLivingCell(1, 1);
+            Program.SetLivingCell(2, 2);
+            Program.CountLivingCells().Should().Be(2);
+            Program.CountAdjacentLivingCells(2, 1).Should().Be(2);
+            Program.CountAdjacentLivingCells(0, 1).Should().Be(1);
+
+        }
     }
 }
