76a05ab0b4620e37ad6fe2e449f6f5ae17eb71b1
ben zina mohamed elyes <benzina.mohamedelyes@gmail.com>
2021-01-20 14:56:07 +0100

    Should accept only ints sperated by a white space (grid size) when user provide string input: Test KO

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -2,11 +2,26 @@
 
 namespace GameOfLife
 {
-    class Program
+    public class Program
     {
+        private int rowCount;
+        private int columnCount;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int generation = 1;
+            string size;
+            do
+            {
+                Console.WriteLine($"Please provide the size of the grid (nb of lines x nb of colomns) separated by a space, exemple: 4 5 for a 4x5 grid");
+                size = Console.ReadLine();
+            }
+            while (VerifyGridSize(size));
+            Console.WriteLine($"Generation {generation}");
         }
+        public static bool VerifyGridSize(string stringSize)
+        {
+            return false;
+        }
+
     }
 }
--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,13 +1,23 @@
 using System;
 using Xunit;
+using GameOfLife;
+using FluentAssertions;
 
 namespace Tests
 {
     public class Tests
     {
         [Fact]
-        public void Test()
+        public void ShouldAcceptOnlyTwoIntsSepareatedByOneSpace()
         {
+            string testString = "78";
+            Program.VerifyGridSize(testString).Should().BeFalse();
+            testString = "7 g";
+            Program.VerifyGridSize(testString).Should().BeFalse();
+            testString = "7 8";
+            Program.VerifyGridSize(testString).Should().BeTrue();
+            testString = "7 8 8";
+            Program.VerifyGridSize(testString).Should().BeFalse();
 
         }
     }
