0ea1b83be067099bff72e9ed00240215aef0a1e8
ben zina mohamed elyes <benzina.mohamedelyes@gmail.com>
2021-01-21 12:19:42 +0100

    Giving an initialized grid when a dead cell has exactly three adjacent living cells then it become alive: Test KO

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -18,7 +18,6 @@ namespace Tests
             Program.VerifyGridSize(testString).Should().BeTrue();
             testString = "7 8 8";
             Program.VerifyGridSize(testString).Should().BeFalse();
-
         }
 
         [Fact]
@@ -56,7 +55,6 @@ namespace Tests
             Program.SetLivingCell(1, 1);
             Program.CountLivingCells().Should().Be(1);
             Program.CountAdjacentLivingCells(2, 1).Should().Be(1);
-
         }
         [Fact]
         public void ShouldFindMultipleLivingAdjacentCell()
@@ -71,10 +69,9 @@ namespace Tests
             Program.CountLivingCells().Should().Be(2);
             Program.CountAdjacentLivingCells(2, 1).Should().Be(2);
             Program.CountAdjacentLivingCells(0, 1).Should().Be(1);
-
         }
         [Fact]
-        public void ACellWithLessThanTwoAdjacentCellsDies()
+        public void ALiveCellWithLessThanTwoAdjacentCellsDies()
         {
             string testString = "5 5";
             Program.VerifyGridSize(testString).Should().BeTrue();
@@ -86,10 +83,9 @@ namespace Tests
             Program.CountLivingCells().Should().Be(2);
             Program.GenerateNextGeneration();
             Program.CountLivingCells().Should().Be(0);
-
         }
         [Fact]
-        public void ACellWithMoreThanThreeAdjacentCellsDies()
+        public void ALiveCellWithMoreThanThreeAdjacentCellsDies()
         {
             string testString = "5 5";
             Program.VerifyGridSize(testString).Should().BeTrue();
@@ -113,11 +109,9 @@ namespace Tests
             Program.IsAlive(2, 0).Should().BeTrue();
             Program.IsAlive(2, 2).Should().BeTrue();
             Program.IsAlive(3, 1).Should().BeTrue();
-
-
         }
         [Fact]
-        public void ACellWithTwoOrThreeAdjacentCellsLives()
+        public void ALiveCellWithTwoOrThreeAdjacentCellsLives()
         {
             string testString = "5 5";
             Program.VerifyGridSize(testString).Should().BeTrue();
@@ -140,8 +134,31 @@ namespace Tests
             Program.IsAlive(1, 1).Should().BeTrue();
             Program.IsAlive(1, 2).Should().BeTrue();
             Program.IsAlive(2, 1).Should().BeTrue();
+        }
+        [Fact]
+        public void ADeadCellWithExactlyThreeAdjacentCellsBecomeAlive()
+        {
+            string testString = "5 5";
+            Program.VerifyGridSize(testString).Should().BeTrue();
 
+            Program.InitializeGridWithDeadCells();
 
+            Program.SetLivingCell(1, 1);
+            Program.SetLivingCell(1, 2);
+            Program.SetLivingCell(2, 1);
+
+            Program.IsDead(2, 2).Should().BeTrue();
+            Program.CountLivingCells().Should().Be(3);
+
+            Program.GenerateNextGeneration();
+
+            Program.CountLivingCells().Should().Be(4);
+
+            Program.IsAlive(2, 2).Should().BeTrue();
+
+            Program.IsAlive(1, 1).Should().BeTrue();
+            Program.IsAlive(1, 2).Should().BeTrue();
+            Program.IsAlive(2, 1).Should().BeTrue();
         }
     }
 }
